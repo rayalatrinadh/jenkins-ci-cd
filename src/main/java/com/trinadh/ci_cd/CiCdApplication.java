c24162cc47fe25d28ce3ff2ac62544bbe1835030
Trinadh Rayala <3nadhmail@gmail.com>
2024-08-23 17:31:27 -0500

    code commit

---
+++ src/main/java/com/trinadh/ci_cd/CiCdApplication.java
@@ -0,0 +1,28 @@
+package com.trinadh.ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CiCdApplication  {
+
+
+	@GetMapping("/greetings")
+	public void hel(){
+		System.out.println("hello");
+	}
+	@GetMapping("/greetings/{name}")
+	public String greetings(@PathVariable("name") String name){
+		return "Hello " + name + " Congratulations you have successfully completed";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdApplication.class, args);
+		System.out.println("hello");
+	}
+}
---
+++ src/test/java/com/trinadh/ci_cd/CiCdApplicationTests.java
@@ -0,0 +1,28 @@
+package com.trinadh.ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+class CiCdApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+
+
+	@Test
+	public void testGreetingsEndpoint() throws Exception{
+		String name = "Trinadh Rayala";
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/greetings/{name}", name))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("Hello "+name + " Congratulations you have successfully completed jenkins CI/CD Demo ..!"));
+	}
+
+}
